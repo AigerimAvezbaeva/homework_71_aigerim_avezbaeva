e7344083da437a11a664c55be33513f7c84ab7e9
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-03-24 00:48:07 +0600

    добавила поля пользователя такие, как пол, номер телефона, описание

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import TextChoices
 
 from accounts.managers import UserManager
+
+
+class GenderChoices(TextChoices):
+    MALE = 'male', 'Мужчина'
+    FEMALE = 'female', 'Женщина'
 
 
 class Account(AbstractUser):
@@ -19,6 +25,25 @@ class Account(AbstractUser):
         blank=True,
         verbose_name='Дата рождения'
     )
+    account_info = models.CharField(
+        max_length=100,
+        verbose_name='Описание профиля',
+        null=True,
+        blank=True
+    )
+    gender = models.CharField(
+        choices=GenderChoices.choices,
+        default=GenderChoices.MALE,
+        verbose_name='Пол',
+        null=True,
+        blank=True,
+        max_length=20
+    )
+    phone_number = models.CharField(
+        verbose_name='Номер телефона',
+        null=True,
+        blank=True,
+        max_length=100)
     liked_posts = models.ManyToManyField(
         verbose_name='Понравившиеся публикации',
         to='posts.Post',
