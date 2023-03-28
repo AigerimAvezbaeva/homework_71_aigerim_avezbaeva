c1f044a4ac2d912bd6cde05e6eebffeff697adbe
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-03-28 09:27:03 +0600

    прописала пути

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from instagram.views import IndexView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('auth/', include('accounts.urls'))
+    path('auth/', include('accounts.urls')),
+    path("", IndexView.as_view(), name='index')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
