3968a2e99aff23a6ffa41e898dd61ce1e67be3d3
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-04-21 03:13:38 +0600

    Добавлено приложение api и его зависимости

--- instagram/settings.py
+++ instagram/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'bootstrap5',
     "posts",
     "accounts",
+    'api',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +122,16 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 
 # Internationalization
--- instagram/urls.py
+++ instagram/urls.py
@@ -23,6 +23,8 @@ from instagram.views import IndexView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('auth/', include('accounts.urls')),
-    path("", IndexView.as_view(), name='index')
+    path("", IndexView.as_view(), name='index'),
+    path('api/', include('api.urls')),
+    path('rest/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
